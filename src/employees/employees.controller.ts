2a182c845169265a755eba0322c75a6b7e5f1f27
txusif <txusif@gmail.com>
2025-02-19 23:53:35 +0530

    integrate the exception filter

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -28,7 +28,10 @@ export class EmployeesController {
   @SkipThrottle({ default: false })
   @Get()
   findAll(@Ip() ip: string, @Query('role') role?: Role) {
-    this.logger.log(`Request for All Employees\t${ip}`);
+    this.logger.log(
+      `Request for All Employees\t${ip}`,
+      EmployeesController.name,
+    );
     return this.employeesService.findAll(role);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   app.enableCors();
   app.setGlobalPrefix('v1');
   await app.listen(3000);
 }
+
 bootstrap();
